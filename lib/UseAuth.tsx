f69b3beb68987bd5c9dde0ce83ca56a0df6e7b40
(cyberpolin) = 'Carlos González <cyberpolin@gmail.com>
2023-05-03 23:09:32 -0600

    done

--- lib/UseAuth.tsx
+++ lib/UseAuth.tsx
@@ -4,7 +4,6 @@ import { IS_LOGGED } from "../pages/login/queries.gql"
 export default () => {
   const res = useQuery(IS_LOGGED)
   const { data } = res
-  console.log("<<<<<< ", data)
 
   return {
     user: data?.authenticatedItem || false,
