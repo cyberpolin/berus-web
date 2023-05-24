0c7f949d10c06b1cb2e760589e5b9e1c833ff276
(cyberpolin) = 'Carlos González <cyberpolin@gmail.com>
2023-05-23 19:14:03 -0600

    done

--- pages/dashboard/areas.tsx
+++ pages/dashboard/areas.tsx
@@ -24,16 +24,7 @@ import "react-datepicker/dist/react-datepicker.css"
 
 const currentDate = "2018-11-01"
 const schedulerData = [
-  {
-    startDate: "2018-11-01T09:45",
-    endDate: "2018-11-01T11:00",
-    title: "Meeting",
-  },
-  {
-    startDate: "2018-11-01T12:00",
-    endDate: "2018-11-01T13:30",
-    title: "Go to a gym",
-  },
+
 ]
 
 const headers = [
