c35071e4aacedeb0d301b5615d3e2a276880ef56
MAC01 <mac01@MAC01s-MacBook-Air.local>
2025-02-28 12:22:32 -0600

    fix flow to upload bill

--- pages/provider/provider-payments.tsx
+++ pages/provider/provider-payments.tsx
@@ -2,6 +2,7 @@ import { useQuery } from '@apollo/client';
 import {
   GET_PROVIDERS_PAYMENTS,
   UPDATE_PROVIDER_PAYMENT_BILL,
+  UPLOAD_PROVIDER_PAYMENT_BILL,
   UPDATE_PROVIDER_PAYMENT_STATUS,
 } from './queries.gql';
 import currency from 'currency.js';
@@ -25,12 +26,17 @@ const PaymentList = () => {
   const { user } = UseAuth();
   const [openBill, setOpenBill] = useState(false);
   const idRef = useRef('');
+  const idBillRef = useRef('');
+  const thereIsImageRef = useRef('');
   const [updateBill, { loading, data, error }] = useMutation(
     UPDATE_PROVIDER_PAYMENT_BILL,
     {
       refetchQueries: [GET_PROVIDERS_PAYMENTS],
     },
   );
+  const [uploadBill, uploadprops] = useMutation(UPLOAD_PROVIDER_PAYMENT_BILL, {
+    refetchQueries: [GET_PROVIDERS_PAYMENTS],
+  });
   const [updatestatus, updatestatusProps] = useMutation(UPDATE_PROVIDER_PAYMENT_STATUS, {
     refetchQueries: [GET_PROVIDERS_PAYMENTS],
   });
@@ -47,12 +53,21 @@ const PaymentList = () => {
   });
 
   const updateBillProvider = async (image: any) => {
-    await updateBill({
-      variables: {
-        id: idRef.current,
-        image: image,
-      },
-    });
+    if (thereIsImageRef.current) {
+      await updateBill({
+        variables: {
+          id: idBillRef.current,
+          image: image,
+        },
+      });
+    } else {
+      await uploadBill({
+        variables: {
+          id: idRef.current,
+          image: image,
+        },
+      });
+    }
     setOpenBill(false);
   };
 
@@ -107,6 +122,18 @@ const PaymentList = () => {
           <table className="min-w-full divide-y divide-gray-200 ">
             <thead className="bg-gray-50">
               <tr>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
+                >
+                  Nombre Proveedor
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
+                >
+                  Tipo Proveedor
+                </th>
                 <th
                   scope="col"
                   className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
@@ -168,8 +195,12 @@ const PaymentList = () => {
                   amountWithTax: number;
                   concept: string;
                   status: string;
+                  provider: { name: string; providerType: string };
+                  bill: { id: string; factura: { publicUrl: string } };
                 }) => (
                   <tr key={payment.id} className="hover:bg-gray-500">
+                    <td className="px-6 py-4">{payment.provider.name}</td>
+                    <td className="px-6 py-4">{payment.provider?.providerType}</td>
                     <td className="px-6 py-4">
                       {payment?.image?.publicUrl && (
                         <a href={payment?.image?.publicUrl} target="_blank">
@@ -210,23 +241,25 @@ const PaymentList = () => {
                           >
                             Rechazar
                           </span>
-                        ) : (
+                        ) : payment.status === 'onTime' ? (
                           <button
                             className="mr-2 rounded bg-emerald-500 px-3 py-1 text-white hover:bg-green-600"
                             onClick={() => router.push(`/provider/${payment.id}`)}
                           >
                             Editar &#9998;
                           </button>
-                        )}
+                        ) : null}
                       </div>
                     </td>
                     {user.isAdmin && (
                       <td className="px-6 py-4">
-                        {payment.status === 'onTime' && (
+                        {payment.status !== 'rejected' && payment?.image?.publicUrl && (
                           <button
                             className="w-full rounded bg-green-500 px-3 py-1 text-white hover:bg-green-600"
                             onClick={() => {
                               idRef.current = payment.id;
+                              idBillRef.current = payment.bill.id;
+                              thereIsImageRef.current = payment.bill.factura.publicUrl;
                               setOpenBill(!openBill);
                             }}
                           >
