4c64699059a9e859d947ac0b736f7e4180cfb265
MAC01 <mac01@MAC01s-MacBook-Air.local>
2025-02-28 09:41:21 -0600

    create querie file to residents tenants

--- pages/tenants-residents/[id].tsx
+++ pages/tenants-residents/[id].tsx
@@ -1,14 +1,14 @@
-import Button from '@/components/Button'
-import Input from '@/components/General/Input'
-import Select from '@/components/General/Select'
-import Layout from '@/components/layout/NLayout'
-import { useFormik } from 'formik'
-import * as yup from 'yup'
-import { CREATE_RESIDENT, CREATE_TENANT } from '../dashboard/queries.gql'
-import { useMutation } from '@apollo/client'
-import { useRouter } from 'next/router'
-import { useState } from 'react'
-import UseAuth from '@/lib/UseAuth'
+import Button from '@/components/Button';
+import Input from '@/components/General/Input';
+import Select from '@/components/General/Select';
+import Layout from '@/components/layout/NLayout';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import { CREATE_RESIDENT, CREATE_TENANT } from './queries.gql';
+import { useMutation } from '@apollo/client';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import UseAuth from '@/lib/UseAuth';
 
 const schemaUser = yup.object().shape({
   name: yup.string().required('Elnombre es requerido'),
@@ -25,7 +25,7 @@ const schemaUser = yup.object().shape({
     .string()
     .oneOf([yup.ref('password'), null], 'Las contraseñas deben coincidir')
     .required('La confirmación de contraseña es requerida'),
-})
+});
 
 const schemaTenants = yup.object().shape({
   address: yup.string().required('La direccion es requerida'),
@@ -34,23 +34,23 @@ const schemaTenants = yup.object().shape({
     .oneOf(['ACTIVE', 'DELINQUENT', 'TERMINATED', 'UNDER_REVIEW'])
     .required('El estado es requerido'),
   properties: yup.string(),
-})
+});
 
 const schemaResidents = yup.object().shape({
   properties: yup.string(),
-})
+});
 
-const userTenants = schemaUser.concat(schemaTenants)
-const userResidents = schemaUser.concat(schemaResidents)
+const userTenants = schemaUser.concat(schemaTenants);
+const userResidents = schemaUser.concat(schemaResidents);
 
 const ResidentTenantsForm = () => {
-  const { id } = useRouter().query
-  const { user } = UseAuth()
-  const tenant = user.tenant?.properties
-  const getProperties = tenant?.[0]?.id ? tenant : user?.owner?.properties
-  const [isTenants, setIsTenants] = useState(false)
-  const [create_resident, create_residentProps] = useMutation(CREATE_RESIDENT)
-  const [create_tenant, create_tenantProps] = useMutation(CREATE_TENANT)
+  const { id } = useRouter().query;
+  const { user } = UseAuth();
+  const tenant = user.tenant?.properties;
+  const getProperties = tenant?.[0]?.id ? tenant : user?.owner?.properties;
+  const [isTenants, setIsTenants] = useState(false);
+  const [create_resident, create_residentProps] = useMutation(CREATE_RESIDENT);
+  const [create_tenant, create_tenantProps] = useMutation(CREATE_TENANT);
   const initialValuesUserTenants = {
     name: '',
     phone: '',
@@ -60,7 +60,7 @@ const ResidentTenantsForm = () => {
     address: '',
     status: 'ACTIVE',
     properties: user?.owner?.properties?.[0]?.id || '',
-  }
+  };
   const initialValuesUserResidents = {
     name: '',
     phone: '',
@@ -68,13 +68,11 @@ const ResidentTenantsForm = () => {
     password: '',
     confirmPassword: '',
     properties: getProperties?.[0]?.id || '',
-  }
+  };
 
   const { values, errors, touched, handleSubmit, setFieldValue, handleChange } =
     useFormik({
-      initialValues: isTenants
-        ? initialValuesUserTenants
-        : initialValuesUserResidents,
+      initialValues: isTenants ? initialValuesUserTenants : initialValuesUserResidents,
       validationSchema: isTenants ? userTenants : userResidents,
       enableReinitialize: true,
       onSubmit: async (variables, { resetForm }) => {
@@ -98,20 +96,20 @@ const ResidentTenantsForm = () => {
                 password: variables.password,
                 properties: variables.properties || user.properties[0].id,
               },
-            })
+            });
         console.log(
           'Error creating ',
-          isTenants ? create_tenantProps.error : create_residentProps.error
-        )
-        resetForm()
+          isTenants ? create_tenantProps.error : create_residentProps.error,
+        );
+        resetForm();
       },
-    })
+    });
 
   if (false) {
-    return <h1>Loading...</h1>
+    return <h1>Loading...</h1>;
   }
 
-  console.log('error', errors)
+  console.log('error', errors);
 
   return (
     <Layout>
@@ -236,7 +234,7 @@ const ResidentTenantsForm = () => {
         </form>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default ResidentTenantsForm
+export default ResidentTenantsForm;
--- pages/tenants-residents/table-tenants-residents.tsx
+++ pages/tenants-residents/table-tenants-residents.tsx
@@ -1,48 +1,45 @@
-import { useQuery } from '@apollo/client'
-import {
-  GET_RESIDENTS_TENANTS,
-  GET_RESIDENTS_BY_TENANT,
-} from '../dashboard/queries.gql'
-import Layout from '@/components/layout/NLayout'
-import { useRouter } from 'next/router'
-import UseAuth from '@/lib/UseAuth'
-import { UPDATE_RESIDENT, UPDATE_TENANT } from '../dashboard/queries.gql'
-import { useMutation } from '@apollo/client'
+import { useQuery } from '@apollo/client';
+import { GET_RESIDENTS_TENANTS, GET_RESIDENTS_BY_TENANT } from '../dashboard/queries.gql';
+import Layout from '@/components/layout/NLayout';
+import { useRouter } from 'next/router';
+import UseAuth from '@/lib/UseAuth';
+import { UPDATE_RESIDENT, UPDATE_TENANT } from './queries.gql';
+import { useMutation } from '@apollo/client';
 
 const TableTenantsResidents = () => {
-  const router = useRouter()
-  const { user } = UseAuth()
-  const [update_resident, update_residentProps] = useMutation(UPDATE_RESIDENT)
-  const [update_tenant, update_tenantProps] = useMutation(UPDATE_TENANT)
-  const resident = user.resident?.properties?.[0]
-  const tenantPropertyID = user.tenant?.properties?.[0]?.id
-  let residestTenantsProps
-  let ownerID = user.owner?.properties?.[0]?.id
+  const router = useRouter();
+  const { user } = UseAuth();
+  const [update_resident, update_residentProps] = useMutation(UPDATE_RESIDENT);
+  const [update_tenant, update_tenantProps] = useMutation(UPDATE_TENANT);
+  const resident = user.resident?.properties?.[0];
+  const tenantPropertyID = user.tenant?.properties?.[0]?.id;
+  let residestTenantsProps;
+  let ownerID = user.owner?.properties?.[0]?.id;
   const handleDelete = (id: string, type: string) => {
     type === 'tenants'
       ? update_tenant({ variables: { id: id, isActive: false } })
-      : update_resident({ variables: { id: id, isActive: false } })
-  }
+      : update_resident({ variables: { id: id, isActive: false } });
+  };
 
   if (tenantPropertyID) {
     residestTenantsProps = useQuery(GET_RESIDENTS_BY_TENANT, {
       variables: {
         id: tenantPropertyID,
       },
-    })
+    });
   } else {
     residestTenantsProps = useQuery(GET_RESIDENTS_TENANTS, {
       variables: {
         id: ownerID,
       },
-    })
+    });
   }
-  const { data, loading, error } = residestTenantsProps
+  const { data, loading, error } = residestTenantsProps;
   const people = tenantPropertyID
     ? { resident: data?.property?.residents }
-    : data?.property
+    : data?.property;
   if (loading) {
-    return
+    return;
   }
   return (
     <Layout>
@@ -51,9 +48,7 @@ const TableTenantsResidents = () => {
         {!resident?.id && (
           <button
             className=" mr-2 mt-3 w-32 rounded bg-emerald-700 px-3 py-1 text-white hover:bg-green-500"
-            onClick={() =>
-              router.push(`/tenants-residents/add-tenant-resident`)
-            }
+            onClick={() => router.push(`/tenants-residents/add-tenant-resident`)}
           >
             {' '}
             Agregar +
@@ -121,10 +116,10 @@ const TableTenantsResidents = () => {
                                   className="mr-2 rounded px-3 py-1 text-red-400  hover:bg-red-500 hover:text-white"
                                   onClick={() => {
                                     const result = window.confirm(
-                                      `Seguro que deseas eliminar a ${person.name}?`
-                                    )
+                                      `Seguro que deseas eliminar a ${person.name}?`,
+                                    );
                                     if (result) {
-                                      handleDelete(person.id, typePerson)
+                                      handleDelete(person.id, typePerson);
                                     }
                                   }}
                                 >
@@ -134,15 +129,15 @@ const TableTenantsResidents = () => {
                             </div>
                           </td>
                         </tr>
-                      )
-                    })
+                      );
+                    }),
                 )}
             </tbody>
           </table>
         </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default TableTenantsResidents
+export default TableTenantsResidents;
