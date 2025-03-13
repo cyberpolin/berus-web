f8a1cadde9aeea74a6d0a7848400d3b2e5c0b483
MAC01 <mac01@MAC01s-MacBook-Air.local>
2025-03-13 14:17:48 -0600

    show winner

--- pages/survey/index.tsx
+++ pages/survey/index.tsx
@@ -1,7 +1,12 @@
 import NLayout from '@/components/layout/NLayout'
 import { useState } from 'react'
 import { useQuery, useMutation } from '@apollo/client'
-import { GET_SURVEY_RECENT, VOTE_SURVEY, GET_VOTE } from './queries.gql'
+import {
+  GET_SURVEY_RECENT,
+  VOTE_SURVEY,
+  GET_VOTE,
+  GET_TOTAL_VOTES,
+} from './queries.gql'
 import UseAuth from '@/lib/UseAuth'
 import { useEffect } from 'react'
 
@@ -16,6 +21,19 @@ export default function Survey() {
     variables: {},
   })
   const survey = surveys?.[0]
+  const [voteSurvey, { loading: voteLoading, error: voteError }] =
+    useMutation(VOTE_SURVEY)
+  const { question1, option1, option2 } = survey?.questions
+    ? JSON.parse(survey.questions)
+    : { question1: '', option1: '', option2: '' }
+
+  const { data: totalVotes = {} } = useQuery(GET_TOTAL_VOTES, {
+    variables: {
+      id: survey?.id,
+      option: option1,
+      option2,
+    },
+  })
   const { data: { votes } = {} } = useQuery(GET_VOTE, {
     variables: {
       id: survey?.id,
@@ -23,17 +41,19 @@ export default function Survey() {
     },
   })
 
+  const getWiner = () => {
+    const { option1: value, option2: value2 } = totalVotes
+    return {
+      [value > value2 ? option1 : option2]: value > value2 ? value : value2,
+    }
+  }
+  const [key, value] = Object.entries(getWiner())[0]
+
   useEffect(() => {
     if (votes && votes.length > 0) {
       setVote(votes[0].vote)
     }
   }, [votes])
-
-  const [voteSurvey, { loading: voteLoading, error: voteError }] =
-    useMutation(VOTE_SURVEY)
-  const { question1, option1, option2 } = survey?.questions
-    ? JSON.parse(survey.questions)
-    : { question1: '', option1: '', option2: '' }
 
   if (loading) return <p>Loading...</p>
 
@@ -59,35 +79,58 @@ export default function Survey() {
 
   return (
     <NLayout>
-      <div className="flex flex-col items-center">
-        <h1 className="text-2xl">{question1}</h1>
-        <div className="mt-4 flex">
+      <div className="mx-auto flex w-[700px] flex-col items-center rounded-lg bg-gray-100 p-6 shadow-lg">
+        <h1 className="mb-6 text-3xl font-bold text-gray-800">{question1}</h1>
+        <div className="mt-4 flex space-x-4">
           <button
-            className={`border-2 px-4 py-2 ${
-              vote === option1 ? 'bg-teal-500' : 'bg-white'
+            className={`rounded-lg border-2 px-6 py-3 transition-all duration-300 ${
+              vote === option1
+                ? 'bg-green-500 text-white'
+                : 'bg-white text-green-500 hover:bg-green-100'
             }`}
-            onClick={() => handleVote(option1)}
+            disabled={!!vote}
+            onClick={() =>
+              confirm(`Seguro que deseas votar por ${option1}?`) &&
+              handleVote(option1)
+            }
           >
             {option1}
           </button>
           <button
-            className={`border-2 px-4 py-2 ${
-              vote === option2 ? 'bg-green-500' : 'bg-white'
+            className={`rounded-lg border-2 px-6 py-3 transition-all duration-300 ${
+              vote === option2
+                ? 'bg-green-500 text-white'
+                : 'bg-white text-green-500 hover:bg-green-100'
             }`}
-            onClick={() => handleVote(option2)}
+            disabled={!!vote}
+            onClick={() =>
+              confirm(`Seguro que deseas votar por ${option2}?`) &&
+              handleVote(option2)
+            }
           >
             {option2}
           </button>
         </div>
         {vote && (
-          <>
-            <span className="text-lg font-bold text-white">
+          <div className="mt-6 text-center">
+            <span className="block text-lg font-bold text-teal-600">
               Gracias por tu voto
             </span>
-            <span className="text-sm italic text-gray-800">
-              La votación está en proceso
-            </span>
-          </>
+            {survey.state === 'ACTIVE' ? (
+              <span className=" italic text-gray-600">
+                La votación está en proceso
+              </span>
+            ) : (
+              <>
+                <span className=" italic text-gray-600">
+                  La votación ha finalizado
+                </span>
+                <span className="mt-2 block text-lg font-semibold text-gray-800">
+                  El ganador es {key} con {value} votos
+                </span>
+              </>
+            )}
+          </div>
         )}
       </div>
     </NLayout>
