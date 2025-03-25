bba4e25c0cfa0a8074a3462bbc37edc1c6b2dab3
MAC01 <mac01@MAC01s-MacBook-Air.local>
2025-03-25 09:32:37 -0600

    create winner component

--- pages/admin/surveys/votes/[id].tsx
+++ pages/admin/surveys/votes/[id].tsx
@@ -1,10 +1,10 @@
 import Layout from '@/components/layout/NLayout'
 import { useRouter } from 'next/router'
 import { useQuery } from '@apollo/client'
-import { GET_VOTES, GET_TOTAL_VOTES } from '../queries.gql'
+import { GET_VOTES } from '../queries.gql'
 import { useEffect } from 'react'
-import PieChart from '@/components/General/PieChart'
 import Table from '@/components/General/Table'
+import Winner from '@/components/Winner'
 const VoteList = () => {
   const { id } = useRouter().query
   const {
@@ -17,27 +17,6 @@ const VoteList = () => {
       id,
     },
   })
-  const { data: { getCountVotes } = {} } = useQuery(GET_TOTAL_VOTES, {
-    variables: {
-      surveyId: id,
-    },
-  })
-
-  const data =
-    getCountVotes?.countVotes?.map((question: any, index: number) => ({
-      labels: question.results.map((result: any) => result.option),
-      datasets: [
-        {
-          label: 'Resultados de la encuesta',
-          data: question.results.map((result: any) => result.count),
-          backgroundColor: Array.from({ length: question.results?.length }).map(
-            () => `#${Math.floor(Math.random() * 16777215).toString(16)}`
-          ),
-          hoverOffset: 4,
-        },
-      ],
-    })) ?? []
-
   useEffect(() => {
     refetch()
   }, [])
@@ -88,29 +67,7 @@ const VoteList = () => {
             }
           )}
         </Table>
-        <div className="mt-7  flex flex-col justify-center rounded-lg bg-white p-4 shadow-md">
-          <h2 className="mb-2 text-2xl font-semibold text-gray-800">
-            Resultados
-          </h2>
-          {getCountVotes.maxVotes.map((question: any, index: number) => (
-            <>
-              <div className="flex max-h-96 flex-col items-center rounded-md bg-gray-50 p-4 pb-14">
-                <p className="mb-4 text-gray-600">
-                  El ganador de la encuesta es:{' '}
-                  <span className="font-bold">
-                    {getCountVotes?.maxVotes[index].maxResults[0]?.option}
-                  </span>{' '}
-                  con{' '}
-                  <span className="font-bold">
-                    {getCountVotes?.maxVotes[index].maxResults[0]?.count}
-                  </span>{' '}
-                  votos
-                </p>
-                <PieChart data={data[index]} />
-              </div>
-            </>
-          ))}
-        </div>
+        <Winner id={id as string} />
       </div>
     </Layout>
   )
--- pages/survey/vote/[id].tsx
+++ pages/survey/vote/[id].tsx
@@ -1,59 +1,14 @@
 import Layout from '@/components/layout/NLayout'
 import { useRouter } from 'next/router'
-import PieChart from '@/components/General/PieChart'
-import { useQuery } from '@apollo/client'
-import { GET_TOTAL_VOTES } from '../queries.gql'
+import Winner from '@/components/Winner'
 const VoteList = () => {
   const { id } = useRouter().query
-  const { data: { getCountVotes } = {} } = useQuery(GET_TOTAL_VOTES, {
-    variables: {
-      surveyId: id,
-    },
-  })
-
-  const data =
-    getCountVotes?.countVotes?.map((question: any, index: number) => ({
-      labels: question.results.map((result: any) => result.option),
-      datasets: [
-        {
-          label: 'Resultados de la encuesta',
-          data: question.results.map((result: any) => result.count),
-          backgroundColor: Array.from({ length: question.results?.length }).map(
-            () => `#${Math.floor(Math.random() * 16777215).toString(16)}`
-          ),
-          hoverOffset: 4,
-        },
-      ],
-    })) ?? []
 
   return (
     <Layout>
       <div className="mx-auto flex w-full max-w-[1400px] flex-col px-4 ">
         <h2 className="font-semi-bold text-2xl">Votos</h2>
-
-        <div className="mt-7  flex flex-col justify-center rounded-lg bg-white p-4 shadow-md">
-          <h2 className="mb-2 text-2xl font-semibold text-gray-800">
-            Resultados
-          </h2>
-          {getCountVotes?.maxVotes.map((question: any, index: number) => (
-            <>
-              <div className="flex max-h-96 flex-col items-center rounded-md bg-gray-50 p-4 pb-14">
-                <p className="mb-4 text-gray-600">
-                  El ganador de la encuesta es:{' '}
-                  <span className="font-bold">
-                    {getCountVotes?.maxVotes[index].maxResults[0]?.option}
-                  </span>{' '}
-                  con{' '}
-                  <span className="font-bold">
-                    {getCountVotes?.maxVotes[index].maxResults[0]?.count}
-                  </span>{' '}
-                  votos
-                </p>
-                <PieChart data={data[index]} />
-              </div>
-            </>
-          ))}
-        </div>
+        <Winner id={id as string} />
       </div>
     </Layout>
   )
