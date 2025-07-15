1645c201013a1e9a44cd79339c5aa0a3e9640f2c
(cyberpolin) = 'Carlos González <cyberpolin@gmail.com>
2025-07-15 13:34:53 -0600

    fix crash

--- pages/admin/overdue.tsx
+++ pages/admin/overdue.tsx
@@ -5,6 +5,7 @@ import { GET_OVERDUE_PROPERTIES } from './adminQueries.gql'
 import Link from 'next/link'
 import * as XLSX from 'xlsx'
 import { saveAs } from 'file-saver'
+import _ from 'lodash'
 
 type Property = {
   id: string
@@ -41,11 +42,11 @@ const OverdueProperties = () => {
         payments: { dueAmount: string | null | undefined }[]
       }) => ({
         ...property,
-        owner: property.owner.name,
-        ownerId: property.owner.id,
-        ownerPhone: property.owner.phone,
-        ownerEmail: property.owner.email,
-        dueMonths: property.payments.length,
+        owner: property.owner?.name,
+        ownerId: property.owner?.id,
+        ownerPhone: property.owner?.phone,
+        ownerEmail: property.owner?.email,
+        dueMonths: property.payments?.length,
         totalDebt: calculateTotalDebt(property.payments),
       })
     ) || []
@@ -121,43 +122,47 @@ const OverdueProperties = () => {
               { title: 'Total adeudado (MXN)' },
             ]}
           >
-            {propertiesFlat?.map((property: Property) => (
-              <tr key={`${property.square}-${property.lot}`}>
-                <td className="px-6 py-4">{property.square}</td>
-                <td className="px-6 py-4">{property.lot}</td>
-                <td className="px-6 py-4">
-                  {property.owner || 'No registrado'}
-                </td>
-                <td className="px-6 py-4">{property.ownerPhone || '--'}</td>
-                <td className="px-6 py-4">{property.ownerEmail || '--'}</td>
-                <td className="px-6 py-4">
-                  <Link href={`/dashboard/cuotas?pretend=${property.ownerId}`}>
-                    <svg
-                      className="h-6 w-6 dark:text-white"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="1.5"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                      aria-hidden="true"
+            {_.orderBy(propertiesFlat, ['square', 'lot'], ['asc', 'asc']).map(
+              (property: Property) => (
+                <tr key={`${property.square}-${property.lot}`}>
+                  <td className="px-6 py-4">{property.square}</td>
+                  <td className="px-6 py-4">{property.lot}</td>
+                  <td className="px-6 py-4">
+                    {property.owner || 'No registrado'}
+                  </td>
+                  <td className="px-6 py-4">{property.ownerPhone || '--'}</td>
+                  <td className="px-6 py-4">{property.ownerEmail || '--'}</td>
+                  <td className="px-6 py-4">
+                    <Link
+                      href={`/dashboard/cuotas?pretend=${property.ownerId}`}
                     >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                      />
-                    </svg>
-                  </Link>
-                </td>
-                <td className="px-6 py-4">{property.dueMonths}</td>
-                <td className="px-6 py-4 font-medium">
-                  $
-                  {property.totalDebt.toLocaleString('es-MX', {
-                    minimumFractionDigits: 2,
-                  })}
-                </td>
-              </tr>
-            ))}
+                      <svg
+                        className="h-6 w-6 dark:text-white"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="1.5"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        aria-hidden="true"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                        />
+                      </svg>
+                    </Link>
+                  </td>
+                  <td className="px-6 py-4">{property.dueMonths}</td>
+                  <td className="px-6 py-4 font-medium">
+                    $
+                    {property.totalDebt.toLocaleString('es-MX', {
+                      minimumFractionDigits: 2,
+                    })}
+                  </td>
+                </tr>
+              )
+            )}
           </Table>
         )}
       </div>
